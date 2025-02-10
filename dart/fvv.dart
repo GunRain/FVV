78dce4580b775accc00170b63533547a9a41b54a
天绾绮奈 <tianwan@oom-wg.dev>
2025-02-10 13:03:17 +0800

    新增功能(Dart)
    新增mapListString2FvvGroup和fvvGroup2MapListString函数，仅供临时使用

---
+++ dart/fvv.dart
@@ -0,0 +1,32 @@
+class FVV {
+  static String mapListString2FvvGroup(String name, Map<String, List<String>> input) {
+    String result = '{\n  $name = {\n';
+    input.forEach((k, l) {
+      result += '    $k = [\n';
+      for (var v in l) {
+        result += '      "$v",\n';
+      }
+      result += '    ];\n';
+    });
+    result += '  };\n}';
+    return result;
+  }
+
+  static Map<String, List<String>> fvvGroup2MapListString(String name, String input) {
+    RegExp regExp = RegExp(r'\b' + RegExp.escape(name) + r'\s*=\s*\{(.*?)\};', dotAll: true);
+    Match? match = regExp.firstMatch(input);
+    Map<String, List<String>> result = {};
+    if (match != null) {
+      String valueContent = match.group(1) ?? '';
+      RegExp keyValueRegExp = RegExp(r'(\w+)\s*=\s*\[\s*(.*?)\s*\];', dotAll: true);
+      Iterable<Match> keyMatches = keyValueRegExp.allMatches(valueContent);
+      for (var keyMatch in keyMatches) {
+        String key = keyMatch.group(1) ?? '';
+        String valuesString = keyMatch.group(2) ?? '';
+        List<String> values = valuesString.split(',').map((e) => e.trim().replaceAll('"', '')).where((e) => e.isNotEmpty).toList();
+        result[key] = values;
+      }
+    }
+    return result;
+  }
+}
