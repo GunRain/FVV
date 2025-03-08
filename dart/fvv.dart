e932005261dd1c74a1698cee2ada0c001b058681
天绾绮奈 <tianwan@oom-wg.dev>
2025-03-08 14:31:40 +0800

    格式化(Dart)

--- dart/fvv.dart
+++ dart/fvv.dart
@@ -1,8 +1,40 @@
 class FVV {
-  static String mapListString2FvvGroup(
-    String name,
-    Map<String, List<String>> input,
-  ) {
+  static String bool2Fvv(String name, bool value) {
+    return '{ $name = $value; }';
+  }
+
+  static String double2Fvv(String name, double value) {
+    return '{ $name = $value; }';
+  }
+
+  static Map<String, List<String>> fvvGroup2MapListString(String name, String input) {
+    RegExp regExp = RegExp(r'\b' + RegExp.escape(name) + r'\s*=\s*\{(.*?)\};', dotAll: true);
+    Match? match = regExp.firstMatch(input);
+    Map<String, List<String>> result = {};
+    if (match != null) {
+      String valueContent = match.group(1) ?? '';
+      RegExp keyValueRegExp = RegExp(r'(\w+)\s*=\s*\[\s*(.*?)\s*\];', dotAll: true);
+      Iterable<Match> keyMatches = keyValueRegExp.allMatches(valueContent);
+      for (var keyMatch in keyMatches) {
+        String key = keyMatch.group(1) ?? '';
+        String valuesString = keyMatch.group(2) ?? '';
+        List<String> values =
+            valuesString.split(',').map((e) => e.trim().replaceAll('"', '')).where((e) => e.isNotEmpty).toList();
+        result[key] = values;
+      }
+    }
+    return result;
+  }
+
+  static String int2Fvv(String name, int value) {
+    return '{ $name = $value; }';
+  }
+
+  static String listString2Fvv(String name, List<String> value) {
+    return '{ $name = [${value.map((v) => '"$v"').join(', ')}]; }';
+  }
+
+  static String mapListString2FvvGroup(String name, Map<String, List<String>> input) {
     String result = '{\n  $name = {\n';
     input.forEach((k, l) {
       result += '    $k = [\n';
@@ -15,55 +47,7 @@ class FVV {
     return result;
   }
 
-  static Map<String, List<String>> fvvGroup2MapListString(
-    String name,
-    String input,
-  ) {
-    RegExp regExp = RegExp(
-      r'\b' + RegExp.escape(name) + r'\s*=\s*\{(.*?)\};',
-      dotAll: true,
-    );
-    Match? match = regExp.firstMatch(input);
-    Map<String, List<String>> result = {};
-    if (match != null) {
-      String valueContent = match.group(1) ?? '';
-      RegExp keyValueRegExp = RegExp(
-        r'(\w+)\s*=\s*\[\s*(.*?)\s*\];',
-        dotAll: true,
-      );
-      Iterable<Match> keyMatches = keyValueRegExp.allMatches(valueContent);
-      for (var keyMatch in keyMatches) {
-        String key = keyMatch.group(1) ?? '';
-        String valuesString = keyMatch.group(2) ?? '';
-        List<String> values =
-            valuesString
-                .split(',')
-                .map((e) => e.trim().replaceAll('"', ''))
-                .where((e) => e.isNotEmpty)
-                .toList();
-        result[key] = values;
-      }
-    }
-    return result;
-  }
-
-  static String listString2Fvv(String name, List<String> value) {
-    return '{ $name = [${value.map((v) => '"$v"').join(', ')}]; }';
-  }
-
   static String string2Fvv(String name, String value) {
     return '{ $name = "$value"; }';
-  }
-
-  static String double2Fvv(String name, double value) {
-    return '{ $name = $value; }';
-  }
-
-  static String int2Fvv(String name, int value) {
-    return '{ $name = $value; }';
-  }
-
-  static String bool2Fvv(String name, bool value) {
-    return '{ $name = $value; }';
   }
 }
