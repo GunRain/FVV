7c700fd0c17f122ab8d5ac796db77a1ce606b49a
天绾绮奈 <tianwan@oom-wg.dev>
2025-03-16 11:25:11 +0800

    FVV应该全大写(Dart)

--- dart/fvv.dart
+++ dart/fvv.dart
@@ -11,23 +11,23 @@
 //================================================================================================================
 
 class FVV {
-  static String bool2Fvv(String name, bool value) {
+  static String bool2FVV(String name, bool value) {
     return '$name = $value';
   }
 
-  static String double2Fvv(String name, double value) {
+  static String double2FVV(String name, double value) {
     return '$name = $value';
   }
 
-  static String int2Fvv(String name, int value) {
+  static String int2FVV(String name, int value) {
     return '$name = $value';
   }
 
-  static String listString2Fvv(String name, List<String> value) {
+  static String listString2FVV(String name, List<String> value) {
     return '$name = [${value.map((v) => '"$v"').join(',')}]';
   }
 
-  static String mapListString2FvvGroup(String name, Map<String, List<String>> input) {
+  static String mapListString2FVVGroup(String name, Map<String, List<String>> input) {
     String result = '$name = {\n';
     input.forEach((k, l) {
       result += '  $k = [\n';
@@ -40,7 +40,7 @@ class FVV {
     return result;
   }
 
-  static String string2Fvv(String name, String value) {
+  static String string2FVV(String name, String value) {
     return '$name = "$value"';
   }
 }
