9ded2ab116ee2d2c8afb321d5e0736bb8e4cc6ff
白彩恋 <shiro@oom-wg.dev>
2026-03-21 22:17:44 +0800

    Update

--- docs/src/lib/mdx.tsx
+++ docs/src/lib/mdx.tsx
@@ -1,41 +1,42 @@
-import * as AccordionComponents from 'fumadocs-ui/components/accordion'
-// import * as BannerComponents from 'fumadocs-ui/components/banner'
-import * as CodeBlockComponents from 'fumadocs-ui/components/codeblock'
-// import * as DynamicCodeBlockComponents from 'fumadocs-ui/components/dynamic-codeblock'
-import * as FilesComponents from 'fumadocs-ui/components/files'
-import * as GithubInfoComponents from 'fumadocs-ui/components/github-info'
-import * as ImageZoomComponents from 'fumadocs-ui/components/image-zoom'
-// import * as InlineTOCComponents from 'fumadocs-ui/components/inline-toc'
-import * as StepsComponents from 'fumadocs-ui/components/steps'
-import * as TabsComponents from 'fumadocs-ui/components/tabs'
-import * as TypeTableComponents from 'fumadocs-ui/components/type-table'
+import * as AccordionWidgets from 'fumadocs-ui/components/accordion'
+import * as CodeBlockWidgets from 'fumadocs-ui/components/codeblock'
+import * as FilesWidgets from 'fumadocs-ui/components/files'
+import * as StepsWidgets from 'fumadocs-ui/components/steps'
+import * as TabsWidgets from 'fumadocs-ui/components/tabs'
+// import {Banner} from 'fumadocs-ui/components/banner'
 import {CodeBlock, Pre} from 'fumadocs-ui/components/codeblock'
+// import {DynamicCodeBlock} from 'fumadocs-ui/components/dynamic-codeblock'
+import {GithubInfo} from 'fumadocs-ui/components/github-info'
 import {ImageZoom} from 'fumadocs-ui/components/image-zoom'
+// import {InlineTOC} from 'fumadocs-ui/components/inline-toc'
+import {TypeTable} from 'fumadocs-ui/components/type-table'
 import defaultMdxComponents from 'fumadocs-ui/mdx'
 import type {MDXComponents} from 'mdx/types'
+import Image from 'next/image'
 import Link from 'next/link'
 import {type ComponentPropsWithoutRef} from 'react'
 
 export const getMDXWidgets = (components?: MDXComponents) =>
 	({
 		...defaultMdxComponents,
-		...AccordionComponents,
-		// ...BannerComponents,
-		...CodeBlockComponents,
+		...AccordionWidgets,
+		// Banner,
+		...CodeBlockWidgets,
 		pre: ({children, ...props}: ComponentPropsWithoutRef<'pre'>) => (
 			<CodeBlock {...props}>
 				<Pre>{children}</Pre>
 			</CodeBlock>
 		),
-		// ...DynamicCodeBlockComponents,
-		...FilesComponents,
-		...GithubInfoComponents,
-		...ImageZoomComponents,
+		// DynamicCodeBlock,
+		...FilesWidgets,
+		GithubInfo,
+		Image,
+		ImageZoom,
 		img: props => <ImageZoom {...(props as any)} />,
-		// ...InlineTOCComponents,
+		// InlineTOC,
 		Link,
-		...StepsComponents,
-		...TabsComponents,
-		...TypeTableComponents,
+		...StepsWidgets,
+		...TabsWidgets,
+		TypeTable,
 		...components
 	}) satisfies MDXComponents
