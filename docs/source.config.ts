50748f5ff2c5d8c445b48a5aab2730e3259d07b6
白彩恋 <shiro@oom-wg.dev>
2026-05-10 02:44:16 +0800

    Update

--- docs/source.config.ts
+++ docs/source.config.ts
@@ -1,22 +1,19 @@
-import { defineConfig, defineDocs, frontmatterSchema, metaSchema } from 'fumadocs-mdx/config'
+import { metaSchema, pageSchema } from 'fumadocs-core/source/schema'
+import { defineConfig, defineDocs } from 'fumadocs-mdx/config'
 import lastModified from 'fumadocs-mdx/plugins/last-modified'
 import { z } from 'zod'
 
 export const docs = defineDocs({
 	dir: 'content',
 	docs: {
-		schema: frontmatterSchema.extend({
+		schema: pageSchema.extend({
 			keywords: z.array(z.string()).optional(),
 			'go-import': z.string().optional(),
 			'go-source': z.string().optional()
 		}),
-		postprocess: {
-			includeProcessedMarkdown: true
-		}
+		postprocess: { includeProcessedMarkdown: true }
 	},
-	meta: {
-		schema: metaSchema
-	}
+	meta: { schema: metaSchema }
 })
 
 export default defineConfig({
