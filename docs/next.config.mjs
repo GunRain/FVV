8d11bd758990927e2a4ea5e5d17436618a295199
白彩恋 <shiro@oom-wg.dev>
2026-05-15 19:29:46 +0800

    Update

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,7 +5,7 @@ export default createMDX()({
 	output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
 	trailingSlash: true,
 	reactStrictMode: true,
-	rewrites: async () => [
+	rewrites: () => [
 		{
 			source: '/:slug*.md',
 			destination: '/llms.md/:slug*.md'
