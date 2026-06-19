bf0f6ec321166bf925b3a1dca757b9ad979b7b5d
白彩恋 <shiro@oom-wg.dev>
2026-06-19 17:17:38 +0800

    Update
    Signed-off-by: 白彩恋 <shiro@oom-wg.dev>

--- docs/src/utils/icon.tsx
+++ docs/src/utils/icon.tsx
@@ -1,25 +1,9 @@
 import { readFileSync } from 'node:fs'
 
-import { ImageResponse } from 'next/og'
 import sharp from 'sharp'
 
 export const getIconImage = async (img: URL, size: { width: number; height: number }) =>
-	new ImageResponse(
-		<div
-			style={{
-				background: 'transparent',
-				width: '100%',
-				height: '100%',
-				display: 'flex',
-				alignItems: 'center',
-				justifyContent: 'center'
-			}}>
-			<img
-				src={new Uint8Array(await sharp(readFileSync(img)).png().toBuffer()).buffer as any}
-				width={size.width}
-				height={size.height}
-				style={{ borderRadius: '4px' }}
-			/>
-		</div>,
-		{ ...size }
+	new Response(
+		new Uint8Array(await sharp(readFileSync(img)).resize(size.width, size.height, { fit: 'contain' }).png().toBuffer()).buffer,
+		{ headers: { 'Content-Type': 'image/png' } }
 	)
