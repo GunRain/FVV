ab0e4779a74e9c78f4fa0803963030677ca693bf
白彩恋 <shiro@oom-wg.dev>
2026-07-14 16:20:49 +0800

    Update
    Signed-off-by: 白彩恋 <shiro@oom-wg.dev>

--- docs/src/lib/source.tsx
+++ docs/src/lib/source.tsx
@@ -47,7 +47,7 @@ export const docsConfig = {
 				items: [
 					{
 						label: '广告',
-						href: 'https://ja7.top/ad'
+						href: 'https://ja7.gal.tf/ad'
 					},
 					{
 						label: 'NGA 开发文档',
